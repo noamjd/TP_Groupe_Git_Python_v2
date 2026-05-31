e87c1e18293b4ff102dca7ccb45455d5b138f72e
Noam JOUDI <noamjoudi@gmail.com>
2026-05-31 20:40:41 +0200

    #

--- catalogue.py
+++ catalogue.py
@@ -1,3 +1,4 @@
+# Lit le fichier ligne par ligne (ignorer la première ligne d'en-tête)
 with open("catalogue.txt", "r", encoding="utf-8") as fichier:
     ligne1 = fichier.readline().split(";")
     ligne2 = fichier.readline().split(";")
@@ -8,12 +9,15 @@ with open("catalogue.txt", "r", encoding="utf-8") as fichier:
 
 lignes = [ ligne2, ligne3, ligne4, ligne5, ligne6]
 
+#Affiche chaque film : "Inception (2010) - Réalisateur : Nolan - Note : 9/10"
 for ligne in lignes:
     print(f"{ligne[0]} ({ligne[1]}) - Réalisateur : {ligne[2]} - Note : {ligne[3]}")
-    
+
+#Affiche uniquement les films avec une note ≥ 9    
 for ligne in lignes:
     if int(ligne[3]) >= 9:
         print(ligne[0])
+        
     
 
     
