1388510596a16e75e6a33b69c24d794856897b69
Noam JOUDI <noamjoudi@gmail.com>
2026-05-31 19:38:59 +0200

    fichier ligne par ligne

--- catalogue.py
+++ catalogue.py
@@ -1,7 +1,9 @@
-try:
-    fichier = open("catalogue.txt", "r", encoding="utf-8")
-    contenu = fichier.read()
-    print(contenu)
-    fichier.close()
-except FileNotFoundError:
-    print("Fichier introuvable")
+with open("catalogue.txt", "r", encoding="utf-8") as fichier:
+    ligne1 = fichier.readline()
+    ligne2 = fichier.readline()
+    ligne3 = fichier.readline()
+    ligne4 = fichier.readline()
+    ligne5 = fichier.readline()
+    ligne6 = fichier.readline()   
+print(ligne2 , ligne3 , ligne4 , ligne5 , ligne6)
+    
