376c127fa849a4053485d729224c0f58a5817c3e
zak <zak071006@gmail.com>
2026-05-31 22:39:32 +0200

    a

--- depenses.py
+++ depenses.py
@@ -1,18 +1,6 @@
 import csv
 
-with open("depenses.csv", "r") encoding="utf-8") as f:
-    lecteur = csv.DictReader(fichier)
+with open("depenses.csv", "r", encoding="utf-8") as f:
+    lecteur = csv.DictReader(f)
     for ligne in lecteur:
-        depense = [
- {"Alimentation", "Courses", "supermarche": 42.50},
- {"Transport", "Ticketmetro": 1.90},
- {"Loisirs", "Cinema":12.00}
- {"Alimentation", "Restaurant", "midi":12.00}
- {"Trasnport", "Essence":55.00}
- {"Loisirs","livre", "python":22.90}
-]
-
-x ====
-total = Alimentation,Courses,supermarche + Transport,Ticket,metro + Loisirs,Cinema + Alimentation,Restaurant,midi + Transport,Essence + Loisirs,Livre,Python
-total = float(total) = 150.100
-            
+        print(f"{ligne['date']} | {ligne['categorie']} | {ligne['description']} | {ligne['montant']} EUR")
