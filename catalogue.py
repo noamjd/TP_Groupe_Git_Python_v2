d3676728791e6351f1f15e2d04e8550c8a12b1f3
Noam JOUDI <noamjoudi@gmail.com>
2026-05-31 20:38:35 +0200

    note>9

--- catalogue.py
+++ catalogue.py
@@ -4,11 +4,16 @@ with open("catalogue.txt", "r", encoding="utf-8") as fichier:
     ligne3 = fichier.readline().split(";")
     ligne4 = fichier.readline().split(";")
     ligne5 = fichier.readline().split(";")
+    ligne6 = fichier.readline().split(";")
 
-lignes = [ligne1, ligne2, ligne3, ligne4, ligne5]
+lignes = [ ligne2, ligne3, ligne4, ligne5, ligne6]
 
 for ligne in lignes:
     print(f"{ligne[0]} ({ligne[1]}) - Réalisateur : {ligne[2]} - Note : {ligne[3]}")
+    
+for ligne in lignes:
+    if int(ligne[3]) >= 9:
+        print(ligne[0])
     
 
     
