df717396dfc568c4041d0ba68e3f35412b150901
Noam JOUDI <noamjoudi@gmail.com>
2026-05-31 20:20:47 +0200

    affiche chaque film

--- catalogue.py
+++ catalogue.py
@@ -1,9 +1,11 @@
 with open("catalogue.txt", "r", encoding="utf-8") as fichier:
-    ligne1 = fichier.readline()
-    ligne2 = fichier.readline()
-    ligne3 = fichier.readline()
-    ligne4 = fichier.readline()
-    ligne5 = fichier.readline()
-    ligne6 = fichier.readline()   
-print(ligne2 , ligne3 , ligne4 , ligne5 , ligne6)
+    ligne1 = fichier.readline().split(";")
+    ligne2 = fichier.readline().split(";")
+    ligne3 = fichier.readline().split(";")
+    ligne4 = fichier.readline().split(";")
+    ligne5 = fichier.readline().split(";")
+lignes = [ligne1, ligne2, ligne3, ligne4, ligne5]
+for ligne in lignes:
+    print(f"{ligne[0]} ({ligne[1]}) - Réalisateur : {ligne[2]} - Note : {ligne[3]}")
     
+
