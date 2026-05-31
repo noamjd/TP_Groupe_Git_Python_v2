f305a29930e1677292b4146ae2ff90b683a53b31
Noam JOUDI <noamjoudi@gmail.com>
2026-05-31 20:21:48 +0200

    inutlie

--- catalogue.py
+++ catalogue.py
@@ -4,8 +4,12 @@ with open("catalogue.txt", "r", encoding="utf-8") as fichier:
     ligne3 = fichier.readline().split(";")
     ligne4 = fichier.readline().split(";")
     ligne5 = fichier.readline().split(";")
+
 lignes = [ligne1, ligne2, ligne3, ligne4, ligne5]
+
 for ligne in lignes:
     print(f"{ligne[0]} ({ligne[1]}) - Réalisateur : {ligne[2]} - Note : {ligne[3]}")
+    
+
     
 
