5de15b1e36e630710b812df4c184ce94ba0426a9
Noam JOUDI <noamjoudi@gmail.com>
2026-06-07 00:39:54 +0200

    absolute cinema

--- catalogue.py
+++ catalogue.py
@@ -1,46 +1,41 @@
+films = []
+
 # Lit le fichier ligne par ligne (ignorer la première ligne d'en-tête)
 with open("catalogue.txt", "r", encoding="utf-8") as fichier:
-    ligne1 = fichier.readline().split(";")
-    ligne2 = fichier.readline().split(";")
-    ligne3 = fichier.readline().split(";")
-    ligne4 = fichier.readline().split(";")
-    ligne5 = fichier.readline().split(";")
-    ligne6 = fichier.readline().split(";")
-
-lignes = [ ligne2, ligne3, ligne4, ligne5, ligne6]
+    next(fichier)
+    for ligne in fichier:
+        ligne = ligne.strip().split(";")
+        films.append(ligne)
+        
 
 #Affiche chaque film : "Inception (2010) - Réalisateur : Nolan - Note : 9/10"
-for ligne in lignes:
-    print(f"{ligne[0]} ({ligne[1]}) - Réalisateur : {ligne[2]} - Note : {ligne[3]}")
+for film in films:
+    print(f"{film[0]} ({film[1]}) - Réalisateur : {film[2]} - Note : {film[3]}/10")
 
-#Affiche uniquement les films avec une note ≥ 9    
-for ligne in lignes:
-    if int(ligne[3]) >= 9:
-        print(ligne[0])
+#Affiche uniquement les films avec une note ≥ 9 
+noteSup =[]   
+for film in films:
+    if int(film[3]) >= 9:
+        noteSup.append(film[0])
+print(f"Les films ayant une note supérieur ou égale a 9 sont : {noteSup}")
         
 #Affiche uniquement les films avec une note ≥ 9
 somme = 0
-for ligne in lignes:
-    somme += int(ligne[3])
-    moyenne = somme/len(lignes)
-print(moyenne)
+for film in films:
+    somme += int(film[3])
+moyenne = somme/len(films)
+print(f"La note moyenne des films est {moyenne}")
 
 #Ajoute un nouveau film de ton choix à la fin du fichier
-fichier = open("catalogue.txt", "a", encoding="utf-8")
-fichier.write("Batman;2008;Nolan;10\n")
-fichier.close()
+with open("catalogue.txt", "a", encoding="utf-8") as fichier:
+    fichier.write("Batman;2008;Nolan;10\n")
+
+films = []
+with open("catalogue.txt", "r", encoding="utf-8") as fichier:
+    next(fichier)
+    for ligne in fichier:
+        films.append(ligne.strip().split(";"))
 
 #Relit et réaffiche le catalogue complet pour confirmer l'ajout
-with open("catalogue.txt", "r", encoding="utf-8") as fichier:
-    ligne1 = fichier.readline().split(";")
-    ligne2 = fichier.readline().split(";")
-    ligne3 = fichier.readline().split(";")
-    ligne4 = fichier.readline().split(";")
-    ligne5 = fichier.readline().split(";")
-    ligne6 = fichier.readline().split(";")
-    ligne7 = fichier.readline().split(";")
-    
-lignes = [ ligne2, ligne3, ligne4, ligne5, ligne6, ligne7]
-
-for ligne in lignes:
-    print(f"{ligne[0]} ({ligne[1]}) - Réalisateur : {ligne[2]} - Note : {ligne[3]}")
+for film in films:
+    print(f"{film[0]} ({film[1]}) - Réalisateur : {film[2]} - Note : {film[3]}")
