7a2b9cf112b2028a76cf81906f782aea595dd791
Noam JOUDI <noamjoudi@gmail.com>
2026-06-07 18:34:09 +0200

    save

--- contacts.py
+++ contacts.py
@@ -1,3 +1,18 @@
+contacts = {}
+
 with open("contacts.txt", "r", encoding="utf-8") as fichier:
     for ligne in fichier:
-        print(ligne)
+        contacts = {ligne}
+        ligne = ligne.split("|")
+        print(f"{ligne[0]} -> {ligne[1]} / {" ".join(ligne[2])} ")
+        
+demande = input("Entrer un contact : ")
+contact = contacts.get(demande)
+
+if contact:
+    print(f"{demande} -> {contact[0]} / {contact[1]}")
+else:
+    print("Contact introuvable")
+        
+
+    
